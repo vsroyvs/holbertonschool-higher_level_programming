e48d5cf2e6d93f8f63d7d3f1750c2279bda62c24
Roy <vargas.sap321@gmail.com>
2023-07-20 18:28:32 -0700

    class Square that defines a square also a Public instance method: def area(self): that returns the current square area

---
+++ python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This is a module for square"""
+
+
+class Square:
+    """This is a class Square"""
+
+    def __init__(self, size=0):
+        """This is a Constructor.
+        Arguments:
+            Size: side of square
+        """
+        if not isinstance(size, int):
+            # check if size is a int - it returns true or false
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """This method returns the area of the square"""
+        return (self.__size * self.__size)
