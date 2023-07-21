4f3a9958fb12cd82f92c0356861ad12497f36467
Roy <vargas.sap321@gmail.com>
2023-07-20 17:51:28 -0700

    class Square that defines a square with private instance attribute and valide some exception with raise

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """This is a module for square"""
 
+
 class Square:
     """This is a class Square"""
 
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         """This is a Constructor.
         Arguments:
             Size: side of square
         """
-        if not isinstance(size, int):  # check if size is a int - it returns true or false
+        if not isinstance(size, int):
+            # check if size is a int - it returns true or false
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
