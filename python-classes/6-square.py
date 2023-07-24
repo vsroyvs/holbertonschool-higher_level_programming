61a99b77107e7f298957a708faf24017a7104d01
Roy <vargas.sap321@gmail.com>
2023-07-23 19:57:59 -0700

    class Square that defines a square,also prints in stdout the square with the character #

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -17,7 +17,7 @@ class Square():
     def size(self):
         """These are getter and setter whose to get/set
         the curret size of the square"""
-        self.__size = size
+        return self.__size
 
     @size.setter
     def size(self, value):
@@ -26,24 +26,26 @@ class Square():
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     @property
     def position(self):
         """These are getter and setter whose to get/set
         the current position of the new square"""
         return self.__position
+
     @position.setter
     def position(self, value):
-        if (not isinstance(value, tuple) or 
-            len(value) != 2 or
-            not all(isinstance(num, int) for num in value) or
-            not all(num >= 0 for num in value)):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
         """This is a method that returns the area of the square"""
         return (self.__size * self.__size)
-    
+
     def my_print(self):
         """Print the square with the # character"""
         if self.__size == 0:
