98c7d18a1d29eb398af040e0eb7a9f9c21398db9
Roy <vargas.sap321@gmail.com>
2023-07-24 21:32:57 -0700

    a class Rectangle that defines a rectangle with two public instance method that returns the area and perimeter

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -42,8 +42,12 @@ class Rectangle:
 
     def area(self):
         """This public instance method returns the area of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return (self.__width * self.__height)
 
     def perimeter(self):
         """This public instance method returns perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return (self.__width*2 + 2*self.__height)
