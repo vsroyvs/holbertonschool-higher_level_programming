61579476aeba2f96951080eee14e454e4385f973
Roy <vargas.sap321@gmail.com>
2023-07-24 20:41:31 -0700

    an empty class Rectangle that defines a rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module represents a rectangle"""
+
+
+class Rectangle:
+    """Create a class called Rectangle"""
+    pass
---
+++ python-more_classes/README.md
@@ -0,0 +1,7 @@
+Python - More Classes and Objects
+* self
+* __init__
+* __str__
+* __repr__
+* __dict__
+* getattr
