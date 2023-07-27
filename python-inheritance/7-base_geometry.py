d0dad20dd5309bfb309b9cc567060af9b4515461
Roy <vargas.sap321@gmail.com>
2023-07-26 20:13:54 -0700

    class BaseGeometry, add Public instance method: def integer_validator() that validate the parameter value

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines an empty class"""
+
+
+class BaseGeometry():
+    """This represent a base geometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """this method will validate a parameter as an int
+        Args:
+            name (str): name of parameter
+            value (int): parameter to validate
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
