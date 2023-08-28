4fd1d649ab4b014c34356ea21f371219ba91f48b
Roy <vargas.sap321@gmail.com>
2023-08-28 16:05:30 -0700

    script that takes in arguments and displays all values in the states, where name matches the argument. Safe from MySQL injections!

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa
+safe from MySQL injection'''
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    db_connection = MySQLdb.connect(
+            host='localhost',
+            user=sys.argv[1],
+            password=sys.argv[2],
+            database=sys.argv[3],
+            port=3306)
+    cursor = db_connection.cursor()
+    cursor.execute(
+            "SELECT `id`, `name` FROM `states`\
+            WHERE `name` = %s\
+            ORDER By `id`", (sys.argv[4],))
+    for state in cursor.fetchall():
+        print(state)
+    cursor.close()
+    db_connection.close()
