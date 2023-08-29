18ee41f7289ada1cd152da33140f7003bbcadc78
Roy <vargas.sap321@gmail.com>
2023-08-28 21:21:59 -0700

    Feat: class definition of a State

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Module that contains the class definition pf a State'''
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True,
+                nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
+
+
+if __name__ == '__main__':
+    pass
